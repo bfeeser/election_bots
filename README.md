2932a910981bb560bc90c3afc3d117faaece0f10
Ben Feeser <benjamin.r.feeser@gmail.com>
2018-11-20 12:56:09 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# election_bots
+
+Finding Twitter bots from the recent midterm elections.
+
+Please put your Twitter Developer Account in `config`.
---
+++ __init__.py
@@ -0,0 +1,10 @@
+from configparser import ConfigParser
+import os
+
+
+def read_config(path="config"):
+    if os.path.exists(path):
+        config = ConfigParser()
+        config.read(path)
+        return config
+    raise RuntimeError(f"Missing {path} config file")
